35bc9797d9cac6d22186e3a02657ab500d2e7bd1
GettyOrawo <gettyorawo@gmail.com>
2019-01-17 15:59:03 +0300

    create patients migration

---
+++ priv/repo/migrations/20190117125648_create_patient.exs
@@ -0,0 +1,22 @@
+defmodule Nailinda.Repo.Migrations.CreatePatient do
+  use Ecto.Migration
+
+  def change do
+    use Ecto.Migration
+
+    def change do
+      create table(:patients) do
+        add :first_name, :string
+        add :last_name, :string
+        add :date_of_birth, :date
+        add :location, :string
+        add :id_number, :integer
+        add :phone_number, :integer
+        add :next_of_kin, :integer
+
+        timestamps()
+      end
+    end
+  end
+end
+end
