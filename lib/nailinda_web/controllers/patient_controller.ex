52413671ff035cb8704d4df7d1776494ed52e46c
Linda Achieng <achielinda44@gmail.com>
2019-02-12 11:02:32 +0300

    Update patient_controller.ex
    fix linting

--- lib/nailinda_web/controllers/patient_controller.ex
+++ lib/nailinda_web/controllers/patient_controller.ex
@@ -19,37 +19,38 @@ defmodule NailindaWeb.PatientController do
      patient= User.get_patient_by_id(id)
      render(conn, "show.html", patient: patient)
    end
+  
+  def create(conn, %{"patient" => patient_params}) do
+    case User.create_patient(patient_params) do
+  	{:ok, %Patient{} = patient} ->
+  	  conn
+  	  |>  put_flash(:info, "#{patient.first_name}  created successfuly")
+  	  |>  redirect(to: "/patients")
 
-	 def create(conn, %{"patient" => patient_params}) do
-  	 	case User.create_patient(patient_params) do
-  	 	{:ok, %Patient{} = patient} ->
-  	 	conn
-  	 	|>  put_flash(:info, "#{patient.first_name}  created successfuly")
-  	 	|>  redirect(to: "/patients")
+  	{:error, %Ecto.Changeset{} = changeset} ->
+  	   render(conn, "new.html", changeset: changeset)
+    end
+  end
+ 
+ def edit(conn, %{"id" => id}) do
+   patient = User.get_patient_by_id(id)
+   changeset = Patient.changeset(patient, %{})
+   render(conn, "edit.html", patient: patient, changeset: changeset)
+ end
 
-  		{:error, %Ecto.Changeset{} = changeset} ->
-  		render(conn, "new.html", changeset: changeset)
-  		end
-	  end
-
-	  def edit(conn, %{"id" => id}) do
-  		patient = User.get_patient_by_id(id)
-  		changeset = Patient.changeset(patient, %{})
-  		render(conn, "edit.html", patient: patient, changeset: changeset)
-	  end
-
-	  def update(conn, %{"id" => id, "patient" => attrs}) do
-  		patient = User.get_patient_by_id(id)
+ def update(conn, %{"id" => id, "patient" => attrs}) do
+    patient = User.get_patient_by_id(id)
 
       case User.update_patient(patient, attrs) do
-  			{:ok, %Patient{}} ->
-  				conn
-  				|> put_flash(:info, "Successfully update")
-  				|>redirect(to: "/")
-  			{:error, %Ecto.Changeset{} = changeset} ->
-  				conn
-  				|>put_flash(:error, "Updating failed")
-  				|> render("edit.html", changeset: changeset, patient: patient)
-  		end
-	  end
+  	{:ok, %Patient{}} ->
+  	   conn
+  	   |> put_flash(:info, "Successfully update")
+  	   |>redirect(to: "/")
+		
+  	{:error, %Ecto.Changeset{} = changeset} ->
+  	   conn
+  	   |>put_flash(:error, "Updating failed")
+  	   |> render("edit.html", changeset: changeset, patient: patient)
+  	end
+   end
 end
