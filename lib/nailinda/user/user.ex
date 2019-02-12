cccc266c29734d2124c9e31a15dc9f3b0f7801bf
Linda Achieng <achielinda44@gmail.com>
2019-02-12 10:57:59 +0300

    Update user.ex
    fix linting

--- lib/nailinda/user/user.ex
+++ lib/nailinda/user/user.ex
@@ -1,21 +1,21 @@
 defmodule Nailinda.User do
-	@moduledoc """
-	This is the patients context
-	"""
-	alias Nailinda.Repo
-	alias Nailinda.User.Patient
+ @moduledoc """
+   This is the patients context
+  """
+  alias Nailinda.Repo
+  alias Nailinda.User.Patient
 
-	def create_patient(attrs) do
-		%Patient{}
-    |>Patient.changeset(attrs)
-    |> Repo.insert()
-	end
+  def create_patient(attrs) do
+    %Patient{}
+     |>Patient.changeset(attrs)
+     |> Repo.insert()
+  end
 
-	def update_patient(patient, attrs) do
-		patient
-		|> Patient.changeset(attrs)
-		|> Repo.update()
-	end
+ def update_patient(patient, attrs) do
+    patient
+      |> Patient.changeset(attrs)
+      |> Repo.update()
+  end
 
   def get_all_patients do
     Patient
@@ -23,7 +23,7 @@ defmodule Nailinda.User do
   end
 
   def get_patient_by_id(id) do
-		Patient
+    Patient
     |> Repo.get(id)
   end
 end
