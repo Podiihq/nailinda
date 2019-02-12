9afa476e7daa44235673d8c4c8b077750820540c
LindaKadz <achielinda44@gmail.com>
2019-02-12 10:53:39 +0300

    fix linting

--- lib/nailinda/user/user.ex
+++ lib/nailinda/user/user.ex
@@ -6,9 +6,9 @@ defmodule Nailinda.User do
 	alias Nailinda.User.Patient
 
 	def create_patient(attrs) do
-    	%Patient{}
-    	|>Patient.changeset(attrs)
-    	|> Repo.insert()
+		%Patient{}
+    |>Patient.changeset(attrs)
+    |> Repo.insert()
 	end
 
 	def update_patient(patient, attrs) do
@@ -17,13 +17,13 @@ defmodule Nailinda.User do
 		|> Repo.update()
 	end
 
-    def get_all_patients do
-        Patient
-    	|> Repo.all
-    end
+  def get_all_patients do
+    Patient
+    |> Repo.all
+  end
 
-    def get_patient_by_id(id) do
-			Patient
-      |> Repo.get(id)
-     end
+  def get_patient_by_id(id) do
+		Patient
+    |> Repo.get(id)
+  end
 end
--- lib/nailinda_web/controllers/patient_controller.ex
+++ lib/nailinda_web/controllers/patient_controller.ex
@@ -4,52 +4,52 @@ defmodule NailindaWeb.PatientController do
   alias Nailinda.User
   alias Nailinda.User.Patient
 
-  def new(conn, _params) do
-
+	def new(conn, _params) do
     changeset = Patient.changeset(%Patient{}, %{})
     render(conn, "new.html", changeset: changeset)
   end
 
   def index(conn, _params) do
-     patients = User.get_all_patients
-     render(conn, "index.html", patients: patients)
-   end
+    patients = User.get_all_patients
+    render(conn, "index.html", patients: patients)
+  end
 
-   def show(conn, %{"id" => id}) do
-     patient= User.get_patient_by_id(id)
-     render(conn, "show.html", patient: patient)
-   end
+  def show(conn, %{"id" => id}) do
+    patient= User.get_patient_by_id(id)
+    render(conn, "show.html", patient: patient)
+  end
 
-	 def create(conn, %{"patient" => patient_params}) do
-  	 	case User.create_patient(patient_params) do
+	def create(conn, %{"patient" => patient_params}) do
+  	case User.create_patient(patient_params) do
   	 	{:ok, %Patient{} = patient} ->
-  	 	conn
-  	 	|>  put_flash(:info, "#{patient.first_name}  created successfuly")
-  	 	|>  redirect(to: "/patients")
+				conn
+				|>  put_flash(:info, "#{patient.first_name}  created successfuly")
+				|>  redirect(to: "/patients")
 
   		{:error, %Ecto.Changeset{} = changeset} ->
-  		render(conn, "new.html", changeset: changeset)
-  		end
-	  end
+  		  render(conn, "new.html", changeset: changeset)
+  	end
+	end
 
-	  def edit(conn, %{"id" => id}) do
-  		patient = User.get_patient_by_id(id)
-  		changeset = Patient.changeset(patient, %{})
-  		render(conn, "edit.html", patient: patient, changeset: changeset)
-	  end
+	def edit(conn, %{"id" => id}) do
+  	patient = User.get_patient_by_id(id)
+  	changeset = Patient.changeset(patient, %{})
+  	render(conn, "edit.html", patient: patient, changeset: changeset)
+	end
 
-	  def update(conn, %{"id" => id, "patient" => attrs}) do
-  		patient = User.get_patient_by_id(id)
+  def update(conn, %{"id" => id, "patient" => attrs}) do
+  	patient = User.get_patient_by_id(id)
 
-      case User.update_patient(patient, attrs) do
-  			{:ok, %Patient{}} ->
-  				conn
-  				|> put_flash(:info, "Successfully update")
-  				|>redirect(to: "/")
-  			{:error, %Ecto.Changeset{} = changeset} ->
-  				conn
-  				|>put_flash(:error, "Updating failed")
-  				|> render("edit.html", changeset: changeset, patient: patient)
-  		end
-	  end
+    case User.update_patient(patient, attrs) do
+  		{:ok, %Patient{}} ->
+  			conn
+  			|> put_flash(:info, "Successfully update")
+				|>redirect(to: "/")
+
+  		{:error, %Ecto.Changeset{} = changeset} ->
+  			conn
+  			|>put_flash(:error, "Updating failed")
+  			|> render("edit.html", changeset: changeset, patient: patient)
+  	end
+	end
 end
