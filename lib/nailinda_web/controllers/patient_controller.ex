90ed0fb1a4dd934fa979c787cac2d955aced07af
LindaKadz <achielinda44@gmail.com>
2019-02-12 16:30:51 +0300

    fix merge conflict

--- lib/nailinda_web/controllers/patient_controller.ex
+++ lib/nailinda_web/controllers/patient_controller.ex
@@ -1,18 +1,17 @@
 defmodule NailindaWeb.PatientController do
   use NailindaWeb, :controller
-  
+
   alias Nailinda.User
   alias Nailinda.User.Patient
 
   def new(conn, _params) do
-    
     changeset = Patient.changeset(%Patient{}, %{})
     render(conn, "new.html", changeset: changeset)
-  end 
-      
+  end
+
 
 	 def create(conn, %{"patient" => patient_params}) do
-	 	case User.create_patient(patient_params) do  
+	 	case User.create_patient(patient_params) do
 	 	{:ok, %Patient{} = patient} ->
 	 	conn
 	 	|>  put_flash(:info, "#{patient.first_name}  created successfuly")
@@ -21,9 +20,9 @@ defmodule NailindaWeb.PatientController do
 
     {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
-    end   
+    end
   end
-  
+
    def index(conn, _params) do
       patients = User.get_all_patients
       render(conn, "index.html", patients: patients)
