5c6faeec19d0053a1c50da6937aabef3e87e51d2
7_5P <76pollet@gmail.com>
2019-03-06 15:47:19 +0300

    add map

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,13 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+ function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+    
