a759117ac78bfb8f75f1180008cfe6a6e81aadc1
7_5P <76pollet@gmail.com>
2019-03-23 10:49:41 +0300

    format file

--- lib/nailinda_web/views/hospital_view.ex
+++ lib/nailinda_web/views/hospital_view.ex
@@ -1,3 +1,11 @@
 defmodule NailindaWeb.HospitalView do
   use NailindaWeb, :view
+
+  alias Nailinda.Facilities.Hospital
+
+  def get_member(%Hospital{location: name} = location) do
+    name
+    |> String.split()
+    |> Enum.at(2)
+  end
 end
