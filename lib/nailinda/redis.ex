9248cc575a55b8ef2a3b803d7f0d7d8cbb553bbf
LindaKadz <achielinda44@gmail.com>
2019-03-25 12:32:36 +0300

    Enable user to be saved on redis

---
+++ lib/nailinda/redis.ex
@@ -0,0 +1,22 @@
+defmodule Nailinda.Redis do
+  use GenServer
+
+  def start_link(opts // []) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def init(:ok) do
+    start_redis()
+    {:ok, %{}}
+  end
+
+  defp start_redis() do
+    {:ok, conn} = Redix.start_link("redis://redis:6379", port: 6379)
+      conn
+  end
+
+  def save_patient_location(long, lat, member) do
+    start_redis()
+    |> Redix.command("GEOADD","patient_location", long, lat, member)
+  end
+end
