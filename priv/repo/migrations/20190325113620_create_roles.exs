7b1d8045eb0ce472467c222c5c960919fd7b7c87
GettyOrawo <gettyorawo@gmail.com>
2019-03-25 16:33:01 +0300

    create roles

---
+++ priv/repo/migrations/20190325113620_create_roles.exs
@@ -0,0 +1,12 @@
+defmodule Nailinda.Repo.Migrations.CreateRoles do
+  use Ecto.Migration
+
+  def change do
+    create table(:roles) do
+      add :role, :string
+
+      timestamps()
+    end
+    create unique_index(:roles, [:role])
+  end
+end
